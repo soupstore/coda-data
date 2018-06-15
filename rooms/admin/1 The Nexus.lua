3f7782aa20b872cd25dbee5f69efad727e7aacfe
nicklanng <nick.lanng@lush.co.uk>
2018-06-15 23:20:28 +0100

    Extract lua to separate file. Start spawn rooms

---
+++ rooms/admin/1 The Nexus.lua
@@ -0,0 +1,7 @@
+function onEnter(characterID)
+  narrate(characterID, "As you enter the Nexus, you feel the power of LUA scripting electrify your every cell.")
+  sleep(2)
+  narrate(characterID, "2 seconds later, you feel even more power. What is this?")
+  sleep(5)
+  narrate(characterID, "spoopy")
+end
